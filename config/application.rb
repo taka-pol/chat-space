5fd5ce851746530b47c3f7921d521cc4a212655d
taka-pol <52143375+taka-pol@users.noreply.github.com>
2020-06-10 11:56:10 +0900

    change-default-lang

--- config/application.rb
+++ config/application.rb
@@ -9,7 +9,8 @@ Bundler.require(*Rails.groups)
 module ChatSpace
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 6.0
+    config.time_zone = 'Tokyo'
+    config.i18n.default_locale = :ja
     config.generators do |g|
       g.stylesheets false
       g.javascripts false
